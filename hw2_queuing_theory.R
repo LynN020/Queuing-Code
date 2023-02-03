a328420edb27bf0518514a60def5bf312efa692e
LynN020 <lynnguyen9322@gmail.com>
2023-02-03 16:03:46 -0500

    start getting queuing info given interarrival and service time T(n) S(n) respectively

---
+++ hw2_queuing_theory.R
@@ -0,0 +1,9 @@
+# homework #2 Queuing Theory 
+# Lyn Nguyen 
+
+# customers
+n <- c(1:20)
+
+# interarrival time (from n to n + 1)
+T_of_n <- c(1, 9, 6, 4, 7, 9, 5, 8, 4, 10, 6, 12, 6, 8, 9, 5, 7, 8, 8, 7)
+length(T_of_n)
