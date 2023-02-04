099365f6859e6d2a1fc25354e8f3c8def22b27f4
LynN020 <lynnguyen9322@gmail.com>
2023-02-03 19:11:02 -0500

    code works if provides S(n) and A(n). But what if inputs are S(n) and T(n)?

--- hw2_queuing_theory.R
+++ hw2_queuing_theory.R
@@ -3,54 +3,6 @@
 
 library(dplyr)
 
-# Problem 1
-# customers
-n <- c(1:20)
-
-# Interarrival time (from n to n + 1)
-T_of_n <- c(1, 9, 6, 4, 7, 9, 5, 8, 4, 10, 6, 12, 6, 8, 9, 5, 7, 8, 8, 7)
-# Service Time
-S_of_n <- c(3, 7, 9, 9, 10, 4, 8, 5, 5, 3, 6, 3, 5, 4, 9, 9, 8, 6, 8, 3)
-
-bookkeeping_input <- data.frame(n, T_of_n, S_of_n)
-
-
-
-#Lecture Example
-n <- c(1:12)
-S_of_n <- c(1, 3, 6, 2, 1, 1, 1, 2, 5, 1, 1, 3)
-A_of_n <- c(0, 2, 3, 6, 7, 8, 12, 14, 19, 20, 24, 26)
-T_of_n <- get_T_of_n(A_of_n)
-bk <- data.frame(n, A_of_n, S_of_n, T_of_n)
-# bk$U_of_n[1] <- 0
-# bk$D_of_n[1] <- bk$U_of_n[1] + bk$S_of_n[1]
-
-# U(n) is wrong 
-bk
-bk$U_of_n <- NA
-bk$D_of_n <- NA
-bk$Wq_of_n <- NA
-bk$W_of_n <- NA
-
-bk$U_of_n[1] <- 0 
-bk$D_of_n[1] <- bk$U_of_n[1] + bk$S_of_n[1]
-bk$Wq_of_n[1] <- bk$U_of_n[1] - bk$A_of_n[1]
-bk$W_of_n[1] <- bk$Wq_of_n[1] + bk$S_of_n[1]
-
-for(i in 2:nrow(bk)){
-    bk$U_of_n[i] <- max((bk$D_of_n[i-1]), bk$A_of_n[i]) 
-    bk
-    bk$D_of_n[i] <- bk$U_of_n[i] + bk$S_of_n[i]
-    bk
-    bk$Wq_of_n[i] <- bk$U_of_n[i] - bk$A_of_n[i]
-    bk
-    bk$W_of_n[i] <- bk$Wq_of_n[i] + bk$S_of_n[i]
-}
-
-
-bk
-
-colSums(bk, na.rm = T)
 
 getEventBasedResults <- function(S_of_n, A_of_n){
 
@@ -61,7 +13,9 @@ getEventBasedResults <- function(S_of_n, A_of_n){
   
   T_of_n <- get_T_of_n(A_of_n)
   
-  output <- data.frame(n = length(A_of_n), A_of_n, S_of_n, T_of_n)
+  n <- length(A_of_n)
+  
+  output <- data.frame(n = 1:n, A_of_n, S_of_n, T_of_n)
   
   # U(1) = time first customer enters service is 0 
   output$U_of_n[1] <- 0 
@@ -79,13 +33,53 @@ getEventBasedResults <- function(S_of_n, A_of_n){
     output$W_of_n[i] <- output$Wq_of_n[i] + output$S_of_n[i]
   }
   
+  # "mean line delay" or "mean time in queue" = Wq
+  Wq <-  sum(output$Wq_of_n)/n
+  
+  # "mean system waiting time" or "mean time in system" = W
+  W <- sum(output$W_of_n)/n
+  
+  # lambda = rate of arrival = mean arrival rate 
+  lambda <- n/output$D_of_n[n]
+  
+  # "average system size" = L 
+  L <-  lambda*W
+  
+  # "average queue size" = Lq
+  Lq <- lambda*Wq
+  
+  # "fraction of time server was idle" = p_idle = 1-p_busy
+  p_busy = L-Lq
+  p_idle = 1-p_busy
   
 
-  result = list(result_table = output, column_sums = colSums(output))
+  result = list(result_table = output, column_sums = colSums(output), 
+                Wq = Wq, W = W, lambda = lambda, L= L, Lq= Lq, p_idle = p_idle)
   return(result)
   
 }
   
+
+
+# Problem 1
+# customers
+n <- c(1:20)
+
+# Interarrival time (from n to n + 1)
+T_of_n <- c(1, 9, 6, 4, 7, 9, 5, 8, 4, 10, 6, 12, 6, 8, 9, 5, 7, 8, 8, 7)
+# Service Time
+S_of_n <- c(3, 7, 9, 9, 10, 4, 8, 5, 5, 3, 6, 3, 5, 4, 9, 9, 8, 6, 8, 3)
+
+bookkeeping_input <- data.frame(n, T_of_n, S_of_n)
+
+
+
+#Lecture Example ======
+n <- c(1:12)
+S_of_n <- c(1, 3, 6, 2, 1, 1, 1, 2, 5, 1, 1, 3)
+A_of_n <- c(0, 2, 3, 6, 7, 8, 12, 14, 19, 20, 24, 26)
+# what if T is provided? 
+T_of_n <- c(2, 1, 3, 1, 1, 4, 2, 5, 1, 4, 2, NA)
 result <- getEventBasedResults(S_of_n, A_of_n)
 result
 
